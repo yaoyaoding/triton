620459e1055d142deebebfbd0c47d36e11d5d4a3
Yaoyao Ding <dingyaoyao.cs@gmail.com>
2023-08-09 01:45:10 -0400

    .

--- python/triton_utils/__init__.py
+++ python/triton_utils/__init__.py
@@ -4,6 +4,8 @@ import atexit
 import os
 import shutil
 
+if 'TRITON_CACHE_DIR' not in os.environ:
+    os.environ['TRITON_CACHE_DIR'] = './triton_cache'
 cache_dir = os.environ['TRITON_CACHE_DIR']
 if os.path.exists(cache_dir):
     shutil.rmtree(cache_dir)
@@ -16,6 +18,22 @@ import sys
 fname = 'triton_stderr.log'
 new_stderr = open(fname, 'w')
 os.dup2(new_stderr.fileno(), sys.stderr.fileno())
+
+def process_content(content):
+    import re
+    # remove (#loc\digit*)
+    content = re.sub(r'loc\(#loc\d+\)', '', content)
+
+    # remove loc(.*)
+    content = re.sub(r'loc\([^:]*:\d+:\d+\)', '', content)
+
+    # remove #loc.*
+    content = re.sub(r'#loc.*', '', content)
+
+    # remove empty lines
+    content = '\n'.join([line for line in content.split('\n') if len(line.strip()) > 0])
+
+    return content
 
 
 def run_at_exit():
@@ -37,9 +55,12 @@ def run_at_exit():
             current_lines.append(line)
         else:
             if len(current_lines) > 0:
-                current_content = '\n'.join(current_lines[1:])
+                head_line = current_lines[0]
+                current_content = ''.join(current_lines[1:])
+                current_content = process_content(current_content)
                 with open(f'ir/{idx}{"*" if previous_content != current_content else ""}.mlir', 'w') as f:
-                    f.writelines(current_lines)
+                    f.write(head_line)
+                    f.write(current_content)
                 previous_content = current_content
                 idx += 1
             current_lines = [line]
